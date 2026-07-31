53c779e01e9e7693fe6f62eb52402a147112532a
⟶⚘Nı̄̈͜ı̄̈ībbʌ Oīı̄̈͜ı̄̈īx𝆺꯭𝅥⎯ <abdullahrx96@gmail.com>
2026-07-31 09:25:10 +0600

    Update uid.js

--- modules/cmds/uid.js
+++ modules/cmds/uid.js
@@ -2,31 +2,32 @@ module.exports = {
   config: {
     name: "uid",
     version: "1.0.0",
-    author: "ArYAN",
-    description: "",
+    author: "rX",
+    description: "show users iD",
     category: "utility",
     cooldowns: 5
   },
 
-  onStart: async function({ api, event, usersData }) {
+  onStart: async function({ api, event, message }) {
     let uid;
 
-
-    if (event.type === "message_reply") {
+    if (event.type === "message_reply" && event.messageReply) {
       uid = event.messageReply.senderID;
-    } else if (Object.keys(event.mentions).length > 0) {
+    } else if (event.mentions && Object.keys(event.mentions).length > 0) {
       uid = Object.keys(event.mentions)[0];
     } else {
       uid = event.senderID;
     }
+    
+    if (event.isE2EE || (typeof event.threadID === "string" && event.threadID.includes("@"))) {
+      return message.reply(`UID: ${uid}`);
+    }
 
     try {
-    
       await api.shareContact(uid, uid, event.threadID, event.messageID);
-
     } catch (error) {
       console.error("Error in UID command:", error);
-      api.sendMessage("Error sharing contact: " + error.message, event.threadID, event.messageID);
+      message.reply(`UID: ${uid}`);
     }
   }
 };
